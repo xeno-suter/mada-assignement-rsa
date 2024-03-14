ad279a6d8ba457f59768385adce328813722aa96
Xeno Suter <xeno.suter@students.fhnw.ch>
2024-03-14 08:42:23 +0100

    feat: store the encrypted message into the cipher file

--- src/IOUtility.java
+++ src/IOUtility.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 public class IOUtility {
     public static String FILE_NAME_PUBLIC_KEY = "pk.txt";
     public static String FILE_NAME_PRIVATE_KEY = "sk.txt";
+    public static String FILE_NAME_ENCRYPTED_MESSAGE = "cipher.txt";
 
     public void writePublicKey(BigInteger n, BigInteger d) {
         writeKey(n, d, FILE_NAME_PUBLIC_KEY);
@@ -19,6 +20,22 @@ public class IOUtility {
         var path = Path.of(fileName);
         try {
             Files.write(path, formatKeyPair(n, k).getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void writeEncryptedMessage(BigInteger[] encrypted) {
+        var path = Path.of(FILE_NAME_ENCRYPTED_MESSAGE);
+        try {
+            var content = new StringBuilder();
+            for (int i = 0; i < encrypted.length; i++) {
+                content.append(encrypted[i]);
+                if (i < encrypted.length - 1) {
+                    content.append(",");
+                }
+            }
+            Files.write(path, content.toString().getBytes());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/Task2.java
+++ src/Task2.java
@@ -13,13 +13,25 @@ public class Task2 {
             System.out.println("Text[" + i + "]: " + text[i]);
         }
         System.out.println("Text: " + new String(text));
+//
+//        // fast exponentiation
+//        var x = bigIntegerUtility.fastExponentiation(
+//                new BigInteger("7"),
+//                new BigInteger("13"),
+//                new BigInteger("11")
+//        );
+//        System.out.println("x: " + x);
 
-        // fast exponentiation
-        var x = bigIntegerUtility.fastExponentiation(
-                new BigInteger("7"),
-                new BigInteger("13"),
-                new BigInteger("11")
-        );
-        System.out.println("x: " + x);
+        // todo use the public key to encrypt the message
+        BigInteger[] encrypted = new BigInteger[text.length];
+        for (int i = 0; i < text.length; i++) {
+            encrypted[i] = bigIntegerUtility.fastExponentiation(
+                    new BigInteger(String.valueOf(text[i])),
+                    new BigInteger("7"),
+                    new BigInteger("11")
+            );
+            System.out.println("Encrypted[" + i + "]: " + encrypted[i]);
+        }
+        ioUtility.writeEncryptedMessage(encrypted);
     }
 }
