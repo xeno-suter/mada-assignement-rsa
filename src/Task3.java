a90032f5af7732b43d52a57c45e07a5b193ce50e
Xeno Suter <xeno.suter@students.fhnw.ch>
2024-03-14 11:09:33 +0100

    feat: use private key to decode the message

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,17 @@
+import java.math.BigInteger;
+
 public class Task3 {
+    public static void main(String[] args) {
+        IOUtility ioUtility = new IOUtility();
+        RSAUtility rsaUtility = new RSAUtility();
+
+        BigInteger[] pKey = ioUtility.readPrivateKey();
+        BigInteger[] cipher = ioUtility.readCipher();
+        BigInteger[] decrypted = new BigInteger[cipher.length];
+        for (int i = 0; i < cipher.length; i++) {
+            System.out.println("Cipher[" + i + "]: " + rsaUtility.decrypt(pKey[0], pKey[1], cipher[i]));
+            decrypted[i] = rsaUtility.decrypt(pKey[0], pKey[1], cipher[i]);
+        }
+        ioUtility.writeDecryptedMessage(decrypted);
+    }
 }
